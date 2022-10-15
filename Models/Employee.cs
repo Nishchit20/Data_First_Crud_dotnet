19f3b48b18f3e337c4756dfb1885f44d2d827a59
Nishchit <shettynishchit20@gmail.com>
2022-10-15 21:41:23 +0530

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCore.Models
+{
+    public partial class Employee
+    {
+        
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Enter the Employee Id")]
+        [StringLength(15, MinimumLength = 4, ErrorMessage = "Name must consist of minimum 4 characters")]
+        public string? EmpName { get; set; }
+        [Required(ErrorMessage = "Enter the Employee Name")]
+        public string? Designation { get; set; }
+    }
+}
---
+++ Models/MVC_EMPContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DotNetCore.Models
+{
+    public partial class MVC_EMPContext : DbContext
+    {
+        public MVC_EMPContext()
+        {
+        }
+
+        public MVC_EMPContext(DbContextOptions<MVC_EMPContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=ELW5146\\SQLEXPRESS;Database=MVC_EMP;Trusted_Connection=True;");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Designation)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("DESIGNATION");
+
+                entity.Property(e => e.EmpName)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("EMP_NAME");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
